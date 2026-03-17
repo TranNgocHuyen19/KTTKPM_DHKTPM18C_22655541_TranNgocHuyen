6b1ae21848b48f69fe5a90f534f7e6a6c5dead2e
TranNgocHuyen19 <120317245+TranNgocHuyen19@users.noreply.github.com>
2026-03-17 22:09:31 +0700

    chore: init design-patterns project

---
+++ design-patterns/src/main/java/iuh/fit/se/Main.java
@@ -0,0 +1,10 @@
+package iuh.fit.se;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Design Pattern Exercises:");
+        System.out.println("1) Singleton: iuh.fit.se.singleton.SingletonMain");
+        System.out.println("2) Factory: iuh.fit.se.factory.FactoryMain");
+        System.out.println("3) Abstract Factory: iuh.fit.se.abstractfactory.AbstractFactoryMain");
+    }
+}
